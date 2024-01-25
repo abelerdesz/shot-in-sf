20e010f261bae1d3e8102acfa60278e5d2b04239
Ábel Erdész <abel.erdesz@gmail.com>
2024-01-25 14:10:46 +0100

    add Movie interface

---
+++ src/types/Movie.ts
@@ -0,0 +1,12 @@
+export interface Movie {
+  title: string
+  release_year: number
+  locations: string
+  production_company: string
+  distributor: string
+  director: string
+  writer: string
+  actor_1: string
+  actor_2: string
+  actor_3: string
+}
