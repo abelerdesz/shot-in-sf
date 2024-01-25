cc857d9a6a88e191939215b52b8931f9f30c64ee
Ábel Erdész <abel.erdesz@gmail.com>
2024-01-25 20:56:02 +0100

    add column toggle functionality

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,58 +1,104 @@
-import { useMemo, useState } from 'react'
-import { StyledTable } from './styles'
-import { v4 } from 'uuid'
+import { useMemo, useState } from "react"
+import { DropdownText, StyledTable } from "./styles"
+import { v4 } from "uuid"
+import { Box, DropdownMenu, Flex, IconButton, Switch } from "@radix-ui/themes"
+import { MixerHorizontalIcon } from "@radix-ui/react-icons"
 
 interface Column {
   key: string
   title: string
 }
 
-type RowData = Record<string, string | number>
+interface RowData {
+  [key: string]: string | number
+}
+
+type RenderedRowData = RowData & { id: string }
 
 interface Props {
   columns: Column[]
   displayedColumns: string[]
-  data: Array<RowData>
+  data: RowData[]
 }
 
-type DisplayedData = Record<string, unknown> & { id: string }
-
 export const Table = (props: Props) => {
-  const [displayedColumns, setDisplayedColumns] = useState<string[]>(
+  const [displayedColumns, setDisplayedColumns] = useState(
     props.displayedColumns
   )
 
-  const displayedData: DisplayedData[] = useMemo(
+  const columns = useMemo(
+    () =>
+      props.columns.filter((column) => displayedColumns.includes(column.key)),
+    [props.columns, displayedColumns]
+  )
+
+  const handleColumnSelect = (e: Event, selectedColumn: Column) => {
+    e.preventDefault()
+
+    if (displayedColumns.includes(selectedColumn.key)) {
+      setDisplayedColumns(
+        displayedColumns.filter((column) => column !== selectedColumn.key)
+      )
+    } else {
+      setDisplayedColumns([...displayedColumns, selectedColumn.key])
+    }
+  }
+
+  const renderedData = useMemo<RenderedRowData[]>(
     () =>
       props.data.map((row) => ({
         ...row,
-        id: v4()
+        id: v4(),
       })),
     [props.data]
   )
 
   return (
-    <StyledTable>
-      <thead>
-        <tr>
-          {displayedColumns.map((colKey) => (
-            <th key={colKey}>
-              {props.columns.find((col) => col.key === colKey)?.title}
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {displayedData.map((row) => (
-          <tr key={row.id}>
-            {displayedColumns.map((colKey) => (
-              <td key={`${row.id}-col-${colKey}`}>
-                {row[colKey] as string | number}
-              </td>
+    <Box position="relative">
+      <Box position="absolute" top="0" right="0">
+        <DropdownMenu.Root dir="rtl">
+          <DropdownMenu.Trigger>
+            <IconButton>
+              <MixerHorizontalIcon width={20} height={20} />
+            </IconButton>
+          </DropdownMenu.Trigger>
+          <DropdownMenu.Content>
+            {props.columns.map((column) => (
+              <DropdownMenu.Item
+                key={column.key}
+                onSelect={(e) => handleColumnSelect(e, column)}
+              >
+                <DropdownText as="label" size="2">
+                  <Flex gap="2">
+                    <Switch checked={displayedColumns.includes(column.key)} />{" "}
+                    {column.title}
+                  </Flex>
+                </DropdownText>
+              </DropdownMenu.Item>
+            ))}
+          </DropdownMenu.Content>
+        </DropdownMenu.Root>
+      </Box>
+      <StyledTable>
+        <thead>
+          <tr>
+            {columns.map((column) => (
+              <th key={column.key}>{column.title}</th>
             ))}
           </tr>
-        ))}
-      </tbody>
-    </StyledTable>
+        </thead>
+        <tbody>
+          {renderedData.map((row) => (
+            <tr key={row.id}>
+              {columns.map((column) => (
+                <td key={`row-${row.id}-col-${column.key}`}>
+                  {row[column.key] as string | number}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </StyledTable>
+    </Box>
   )
 }
--- src/components/Table/styles.ts
+++ src/components/Table/styles.ts
@@ -1,5 +1,10 @@
-import styled from '@emotion/styled'
+import styled from "@emotion/styled";
+import { Text } from "@radix-ui/themes";
 
 export const StyledTable = styled.table`
   width: 100%;
-`
+`;
+
+export const DropdownText = styled(Text)`
+  pointer-events: none;
+`;
