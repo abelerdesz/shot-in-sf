40f12b77e2d77e35aa39d73458de7724aa6739d5
Ábel Erdész <abel.erdesz@gmail.com>
2024-01-26 08:41:19 +0100

    create overrides.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
+import './styles/index.css'
 import '@radix-ui/themes/styles.css'
+import './styles/overrides.css'
 import { TableDemo } from './pages/TableDemo'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
